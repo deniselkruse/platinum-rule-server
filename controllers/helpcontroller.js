f04fb7f8ad881b1d4db672868b41f54a6f66b402
Denise Kruse <deniselkruse@gmail.com>
2021-01-19 13:46:22 -0500

    updated help controller

--- controllers/helpcontroller.js
+++ controllers/helpcontroller.js
@@ -15,7 +15,7 @@ router.post('/create', validateSession, (req, res) => {
         firstName: req.user.firstName, // This auto populates
         lastInitial: req.user.lastName, // This auto populates; add code to grab first initial only
         owner: req.user.id, // Double check that this populates
-        title: req.body.help.title,
+        title: req.body.help.title, // Dropdown menu
         description: req.body.help.description,
         availability: req.body.help.availability,
         instances: req.body.help.instances,
@@ -34,7 +34,15 @@ router.post('/create', validateSession, (req, res) => {
         .catch(err => res.status(500).send(err)) 
 });
 
+/**************************
+****VIEW ALL HELP POSTS*****
+***************************/ 
 
+router.get("/", (req, res) => {
+    helpPost.findAll()
+        .then(help => res.status(200).json(help))
+        .catch(err => res.status(500).json({ error: err }))
+});
 
 
 module.exports = router;
