6eca5943392e8aab7de0af83baf7758162a50221
Denise Kruse <deniselkruse@gmail.com>
2021-01-26 21:31:00 -0500

    updated controllers

--- controllers/helpcontroller.js
+++ controllers/helpcontroller.js
@@ -6,11 +6,11 @@ const Help = require('../db').import('../models/help')
 
 /**************************
 ****CREATE A HELP POST*****
-***************************/ 
+***************************/
 
-router.post('/create', validateSession, (req, res) => { 
-    console.log(req.body) 
-    const helpPost = { 
+router.post('/create', validateSession, (req, res) => {
+    console.log(req.body)
+    const helpPost = {
         title: req.body.help.title, // Dropdown menu
         description: req.body.help.description,
         availability: req.body.help.availability,
@@ -20,29 +20,63 @@ router.post('/create', validateSession, (req, res) => {
         userId: req.user.id
     }
 
-    Help.create(helpPost) 
+    Help.create(helpPost)
         .then(help => {
-            res.json({ 
+            res.json({
                 help: help,
                 message: "Post created successfully.",
             })
         })
-        .then(help => res.status(200).json(help)) 
-        .catch(err => res.status(500).send(err)) 
+        .then(help => res.status(200).json(help))
+        .catch(err => res.status(500).send(err))
 });
 
 /**************************
 ****VIEW ALL HELP POSTS*****
-***************************/ 
+***************************/
 
 router.get("/", (req, res) => {
-    Help.findAll({include: ['user']})
+    Help.findAll({ include: ['user'] })
         .then(help => res.status(200).json(help))
         .catch(err => res.status(500).json({ error: err }))
 });
 
+/******************************
+****EDIT HELP POST BY USER*****
+******************************/
 
+router.put('/:username', validateSession, (req, res) => {
+    const updateHelpPost = {
+        title: req.body.help.title,
+        description: req.body.help.description,
+        availability: req.body.help.availability,
+        instances: req.body.help.instances,
+        date: req.body.help.date,
+        inactiveDate: req.body.help.inactiveDate,
+        userId: req.user.id
+    }
 
+    const query = { where: { userId: req.params.userId } };
+
+    Help.update(updateHelpPost, query)
+        .then((helpPost) => res.status(200).json(helpPost))
+        .catch(err => res.status(500).json({ error: err }))
+});
+
+/********************************
+****DELETE HELP POST BY USER*****
+********************************/
+
+router.delete('/:userId', validateSession, async (req, res) => {
+    try {
+        const result = await Help.destroy({
+            where: { userId: req.params.userId }
+        });
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+})
 
 
 module.exports = router;
--- controllers/recipientcontroller.js
+++ controllers/recipientcontroller.js
@@ -4,9 +4,9 @@ const validateSession = require('../middleware/validate-session');
 
 const Recipient = require('../db').import('../models/recipient')
 
-/*****************************
-****CREATE RECIPIENT POST*****
-******************************/
+/****************************
+****CREATE REQUEST POST******
+*****************************/
 
 router.post('/create', validateSession, (req, res) => {
     console.log(req.body)
@@ -32,14 +32,50 @@ router.post('/create', validateSession, (req, res) => {
 });
 
 /********************************
-****VIEW ALL RECIPIENT POSTS*****
+****VIEW ALL REQUEST POSTS*******
 ********************************/
 
 router.get("/", (req, res) => {
-    Recipient.findAll({include: ['user']})
+    Recipient.findAll({ include: ['user'] })
         .then(recipient => res.status(200).json(recipient))
         .catch(err => res.status(500).json({ error: err }))
 });
 
+/*********************************
+****EDIT REQUEST POST BY USER*****
+**********************************/
+
+router.put('/:username', validateSession, (req, res) => {
+    const updateRecipientPost = {
+        title: req.body.help.title,
+        description: req.body.help.description,
+        availability: req.body.help.availability,
+        instances: req.body.help.instances,
+        date: req.body.help.date,
+        inactiveDate: req.body.help.inactiveDate,
+        userId: req.user.id
+    }
+
+    const query = { where: { userId: req.params.userId } };
+
+    Recipient.update(updateRecipientPost, query)
+        .then((recipientPost) => res.status(200).json(recipientPost))
+        .catch(err => res.status(500).json({ error: err }))
+});
+
+/***********************************
+****DELETE REQUEST POST BY USER*****
+************************************/
+
+router.delete('/:userId', validateSession, async (req, res) => {
+    try {
+        const result = await Recipient.destroy({
+            where: { userId: req.params.userId }
+        });
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+})
 
 module.exports = router;
